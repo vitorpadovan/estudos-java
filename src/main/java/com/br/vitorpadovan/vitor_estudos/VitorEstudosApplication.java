1012443cf30f2345613f1bfed20a155a49da029a
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-09-03 22:46:48 -0300

    Commit inicial

---
+++ src/main/java/com/br/vitorpadovan/vitor_estudos/VitorEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.br.vitorpadovan.vitor_estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitorEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitorEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/vitorpadovan/vitor_estudos/VitorEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vitorpadovan.vitor_estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitorEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
